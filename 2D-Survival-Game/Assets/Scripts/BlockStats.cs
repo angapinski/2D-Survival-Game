5c30ead3093bd5d37bf251824e0a59d3a878de31
Andrew Gapinski <angapinski@unomaha.edu>
2018-11-18 12:17:21 -0600

    Created the BlockStats class that each block will inherit

---
+++ 2D-Survival-Game/Assets/Scripts/BlockStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInfo : MonoBehaviour {
+
+    public int blockHealth = 100;
+    public int requiredPickaxeStrength = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
